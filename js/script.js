0d110c34d92da288098a5921defb616cfe3437e7
Chris Goodwin <69440123+cm-goodwin@users.noreply.github.com>
2020-11-12 22:24:43 -0500

    functionality
    add new list item

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-//add a class
-let inputs = $("input");
-inputs.addClass("my-class");
-//Create List/variable using jquery
-let list = $("#list");
-//add storing the <li></li> into a variable
-let li = $("<li></li>");
-//getting the value of an input from the html
-//$("#input").val();
-//store the value into a variable we can use
-let inputValue = $("#input").val();
-//one way to add items to list
-list.append("<li>Apple</li>");
-list.append("<li>Pineapple</li>");
-list.append("<li>Strawberry</li>");
-//other way to add items to list
-list.append(li);
-li.append("potatoes");
+function newItem() {
+  //Adding a new item to the list
+  let list = $("#list");
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
 
-li.append(inputValue);
-list.append(li);
-console.log(list);
+  if (inputValue === "") {
+    alert("You must enter a new value!");
+  } else {
+    list.append(li);
+  }
+}
